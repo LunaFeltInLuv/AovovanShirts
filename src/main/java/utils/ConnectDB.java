3b9f3118da20e3b59fd9dd0d4ce80aa5ce61c97e
LunaFeltInLuv <mself8389@gmail.com>
2026-08-06 08:17:42 +0700

    Fixed ConnectDB

--- src/main/java/utils/ConnectDB.java
+++ src/main/java/utils/ConnectDB.java
@@ -2,29 +2,40 @@ package utils;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class ConnectDB {
-    public static Connection con = null;
+
+    private static final String URL = "jdbc:sqlserver://localhost:1433;"
+            + "databaseName=aovovan_db;"
+            + "encrypt=true;"
+            + "trustServerCertificate=true";
+
+    private static final String USER = "sa";
+    private static final String PASSWORD = "1234";
 
     public static Connection getConnect() {
-        // Thay tên database, tài khoản và mật khẩu theo máy của nhóm.
-        String strDbUrl = "jdbc:sqlserver://localhost:1433;"
-                + "databaseName=aovovan_db;"
-                + "user=sa;password=1234;"
-                + "encrypt=true;trustServerCertificate=true";
 
         try {
-            con = DriverManager.getConnection(strDbUrl);
+
+            Class.forName(
+                    "com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
+            Connection conn = DriverManager.getConnection(
+                    URL,
+                    USER,
+                    PASSWORD);
+
             System.out.println("Kết nối thành công");
-        } catch (SQLException e) {
-            System.err.println("Kết nối lỗi: " + e.getMessage());
+
+            return conn;
+
+        } catch (Exception e) {
+
+            System.out.println("Kết nối thất bại");
+
+            e.printStackTrace();
+
+            return null;
         }
-
-        return con;
-    }
-
-    public static void main(String[] args) {
-        ConnectDB.getConnect();
     }
 }
